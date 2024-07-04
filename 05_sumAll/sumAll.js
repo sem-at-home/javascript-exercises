8ac28b816fec2f313285547430e32889c5c7b8db
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-04 23:49:38 +0200

    Solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if ([min, max].some(invalidInput)) return "ERROR"
+    if (min > max) [min, max] = [max, min]
+    let sum = 0
+    for (i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
 };
+
+function invalidInput(value) {
+    return typeof value != 'number' || value < 0
+}
 
 // Do not edit below this line
 module.exports = sumAll;
