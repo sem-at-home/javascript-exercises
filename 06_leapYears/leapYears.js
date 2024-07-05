370a31c07b254cf5a59184dd8f3ee244fd3e0a54
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-05 16:04:07 +0200

    Solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return yearIsDivisibleBy(year, 4) && !yearIsDivisibleBy(year, 100) || yearIsDivisibleBy(year, 400)
 };
+
+function yearIsDivisibleBy(year, number) {
+    return year % number === 0
+}
+
 
 // Do not edit below this line
 module.exports = leapYears;
