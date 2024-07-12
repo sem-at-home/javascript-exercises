e4754a8425c7d31ab0e9fabe7329e0b0552ea46d
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-12 13:53:43 +0200

    Added Solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, current) => accumulator + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, current) => accumulator * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1
+	for (let i = 1; i <= a; i++) {
+    factorial *= i
+  }
+  return factorial
 };
 
 // Do not edit below this line
