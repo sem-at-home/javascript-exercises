9b155bddd3b03b170ac271c52b25539cb96247c7
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-12 14:11:33 +0200

    Added solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    const cleanedSentence = sentence
+        .toLowerCase()
+        .split('')
+        .filter(filterLettersAndNumbers)
+        .join('')
+    const reversedSentence = cleanedSentence.split('').toReversed().join('')
+    return reversedSentence === cleanedSentence
 };
+
+function filterLettersAndNumbers(char) {
+        return /[a-z0-9]/.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
