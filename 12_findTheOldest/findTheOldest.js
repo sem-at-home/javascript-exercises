eeec1f3789e9b8103f1f99983c4dde93c6701308
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-12 15:09:57 +0200

    Added solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((eldest, person) => getAge(eldest) > getAge(person) ? eldest : person )
 };
+
+function getAge(person) {
+    return (person.yearOfDeath || 2024) - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
