a594b50a36c6d151c8f4817de56e26390f08833a
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-12 14:49:29 +0200

    Added solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    const number = Number(input)
+
+    if (number === 0) return 0 
+    if (number < 0) return "OOPS"
+
+    let oldValue = 1
+    let newValue = 1
+    let fibonacci = 1
+    for ( let i = 2; i < number; i++) {
+        fibonacci = oldValue + newValue
+        oldValue = newValue
+        newValue = fibonacci
+    }
+    return fibonacci
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
